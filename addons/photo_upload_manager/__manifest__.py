a7c3495f27c1b4b03a1c9d716397b415dd70b281
Mboria2000 <boria.1887051@studenti.uniroma1.it>
2025-03-01 14:39:44 +0100

    correzione ereditarietà prodotto-carrello

--- addons/photo_upload_manager/__manifest__.py
+++ addons/photo_upload_manager/__manifest__.py
@@ -3,13 +3,17 @@
     'version': '1.0',
     'summary': 'Gestione upload immagini su prodotti con conferma automatica',
     'description': """
-        Modulo per caricare immagini personalizzate sugli ordini e gestire
-        la conferma automatica delle immagini in base all'ordine associato.
-        Vengono create due viste: una per le immagini non confermate e una per le immagini confermate.
+        Modulo per caricare immagini personalizzate sugli ordini
+        e gestire la conferma automatica delle immagini.
     """,
     'category': 'Website',
     'author': 'Tuo Nome',
-    'depends': ['sale', 'sale_management', 'website_sale'],
+    'depends': [
+        'sale',
+        'sale_management',
+        'website_sale',
+        
+    ],
     'data': [
         'security/ir.model.access.csv',
         'views/photo_order_views.xml',
